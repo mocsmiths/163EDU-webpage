f7caf9823916a2aa7691039fbff090d77abab8ec
mocsmiths <mocsmiths@gmail.com>
2017-06-20 22:09:00 +0800

    视频模块完成

--- qddzy.js
+++ qddzy.js
@@ -15,3 +15,20 @@ var tips_module = (function(){
     });
 
 })();
+// 视频模块
+var video_module = (function(){
+    var videoMask = document.querySelector(".m-video");
+    var videoPlay = document.querySelector(".j-play");
+    var closeVideo = document.querySelector(".video1 .closed");
+    var video = document.querySelector(".video1 video");
+
+    addClickEvent(videoPlay,function(event){
+        videoMask.style.display='block';
+    });
+    addClickEvent(closeVideo,function(event){
+        videoMask.style.display = 'none';
+        if (video && !video.paused){
+            video.pause();
+        };
+    });
+})();
