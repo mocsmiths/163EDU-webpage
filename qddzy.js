3d6d7e6cac61d93c6eaf52b844b9994931c0fae6
mocsmiths <mocsmiths@gmail.com>
2017-06-20 20:09:48 +0800

    顶部取消成功

--- qddzy.js
+++ qddzy.js
@@ -1,4 +1,17 @@
-function canceltip(color) {
-    var ct=document.querySelector('.tip')
-    ct.style.background=color;
-    }
+var tips_module = (function(){
+
+    var tips = document.querySelector('.tip');
+    var closeTip = document.querySelector('.notip');
+
+    var cookie = getCookie();
+
+    if (!cookie.noTips) {
+        tips.style.display = 'block';
+    };
+
+    addClickEvent(closeTip,function(event){
+        setCookie('noTip',1,new Date(9999,9));
+        tips.style.marginTop = '-30px';
+    });
+
+})();
