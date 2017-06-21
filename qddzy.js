c76b99fecfb9e7b9fea14bd3212fc18c3476fdec
mocsmiths <mocsmiths@gmail.com>
2017-06-21 20:02:48 +0800

    轮播图效果完成

--- qddzy.js
+++ qddzy.js
@@ -32,3 +32,58 @@ var video_module = (function(){
         };
     });
 })();
+// 轮播图
+var slide_module = (function(){
+    window.onload = function(){
+        var oDiv = document.querySelector('.m-banner');
+        var oUl = oDiv.getElementsByTagName('ul')[1];
+        var aLi = oUl.getElementsByTagName('li');
+        var aImg = oUl.getElementsByTagName('img');
+
+        var oBtn = document.querySelector('.m-banner .nav');
+        var aA = oBtn.getElementsByTagName('a');
+        var iNow = 0;
+        var Runing = true;
+
+        for (var i =0;i < aA.length;i++) {
+            aA[i].index = i;
+            aA[i].onclick = function(){
+                for (var i = 0;i< aA.length;i++){
+                    aA[i].parentNode.className = '';
+                    aLi[i].className = '';
+                };
+                this.parentNode.className = 'selected';
+                ali[this.index].className = 'show';
+                iNow = this.index;
+            }
+        };
+        var intervalId = setInterval(toRun,5000);
+
+        function toRun(){
+            if (iNow == aLi.length-1){
+                iNow = 0;
+            }
+            else{
+                iNow++;
+            }
+            for (var i = 0;i < aA.length;i++){
+                aA[i].parentNode.className = '';
+                aLi[i].className = '';
+            };
+            aA[iNow].parentNode.className = 'selected';
+            aLi[iNow].className = 'show';
+        }
+        oDiv.onmouseover = function(){
+            if(Runing){
+                clearInterval(intervalId);
+                Runing = false;
+            }
+        };
+        oDiv.onmouseout = function(){
+            if(!Runing){
+                intervalId = setInterval(toRun,5000);
+                Runing = true;
+            }
+        };
+    }
+})();
